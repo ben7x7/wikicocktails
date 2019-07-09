672253f33bdf0e9c5831f350d43a6cb6f1393144
benoit bessiere <benoit.bessiere@gmail.com>
2019-07-09 10:49:08 +0200

    generate Models Cocktail Ingredient Dose

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+# require "open-uri"
+# require "yaml"
+
+# file = "https://raw.githubusercontent.com/ben7x7/MisterCocktail/master/drinks.yml"
+# sample = YAML.load(open(file).read)
+
+# puts 'creating ingredients'
+# sample["ingredients"].each do |ingredient|
+#   Ingredient.create! ingredient
+# end
+
+# puts 'Finished'
