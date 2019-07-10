c3c96480718e327bc40c97fb08c25f27856f3fd6
benoit bessiere <benoit.bessiere@gmail.com>
2019-07-10 09:56:52 +0200

    route controller view for Reviews

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
    resources :cocktails, only: [:index, :show, :create, :new] do
     resources :doses, only: [:create, :new]
+    resources :reviews, only: [:create]
   end
   resources :doses, only: [:destroy]
 
