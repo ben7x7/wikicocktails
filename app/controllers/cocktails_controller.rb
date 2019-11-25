09d3680444aad73fd6520c47184b150bc58b1f94
benoit bessiere <benoit.bessiere@gmail.com>
2019-11-25 17:23:53 +0100

    update gem loofah

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -34,13 +34,14 @@ class CocktailsController < ApplicationController
   def update
      @cocktail = Cocktail.find(params[:id])
      @cocktail.update(cocktail_params)
+
      redirect_to cocktail_path(@cocktail)
   end
 
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :technique)
   end
 
 end
